be64846924699cace2965a1adf74730109d0a5ec
brianmituka <mitukab@outlook.com>
2017-05-21 12:33:56 +0300

    added a readme.txt

---
+++ readme.txt
@@ -0,0 +1,18 @@
+requirements
+1.pip
+install pip by running:
+python get-pip.py  this will either update pip or install it if you dont have it
+ 
+2.pandas
+install pandas through:
+ sudo pip install pandas (failiure to run with sudo you may get some red lines with errors) 
+
+Assumption
+The asterix that appeared alongside some values should be ignored
+
+
+run the script from your terminal with python weather.py
+
+
+
+
--- weather.py
+++ weather.py
@@ -1,8 +1,29 @@
-import pandas as pd
 
-df = pd.read_csv('weather.dat', sep='\s+')
-df[['MxT', 'MnT']] = df[['MxT', 'MnT']].apply(lambda x: x.str[:2].astype(int))
-a = df.MxT - df.MnT
-b = a.index[a==max(a)].tolist()
-print df.loc[b][['Dy', 'MxT', 'MnT']].unstack().tolist()
+import pandas as pd
+import numpy as np
+
+file_read = 'weather.dat'
+
+# use sep=\s+(regex) to use varying whitespace as the separator 
+data = pd.read_table(file_read, sep='\s+')
+
+df = pd.DataFrame(data)
+
+# replace the asterix with an empty space
+df = df.replace('\*','',regex=True)
+
+# change to numerals
+df = df.apply(pd.to_numeric, errors='coerce')
+
+# get the difference between 'Mxt' and 'Mnt'
+df['diff'] = df['MxT'] - df['MnT'] 
+
+# store the maximum spread and the day
+max_diff = df['diff'].max()
+
+max_day = df.loc[df['diff'] == max_diff, 'Dy'].iloc[0]
+
+#removing the decimal points.
+print '{0:g}'.format(float(max_day)), '{0:g}'.format(float(max_diff))
+
 
